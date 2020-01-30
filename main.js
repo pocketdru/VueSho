a5574ca0a2d86702c9be1108685d2e388fb34cf6
pocketdru <pocketdru@gmail.com>
2020-01-29 18:49:10 -0700

    conditional rendering

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el: "#app",
     data: {
         product: "Donuts",
-        image: "./assets/51UDInCwZxL._SL500_.jpg"
+        image: "./assets/51UDInCwZxL._SL500_.jpg",
+        onSale: false,
+        inStock: false,
+        inventory: 0
     }
 })
