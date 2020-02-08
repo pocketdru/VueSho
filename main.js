03063eb6302988de16f3d67ecbadd749059215d5
pocketdru <pocketdru@gmail.com>
2020-02-07 23:16:32 -0700

    style and classes binding

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var app = new Vue({
         product: "Donuts",
         image: "./assets/51UDInCwZxL._SL500_.jpg",
         onSale: false,
-        inStock: true,
+        inStock: false,
         inventory: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
