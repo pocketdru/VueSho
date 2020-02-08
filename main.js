89f098fc5b3ef83502ad0425b36a80e329da0f45
pocketdru <pocketdru@gmail.com>
2020-02-07 23:46:39 -0700

    computed propreties

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 var app = new Vue({
     el: "#app",
     data: {
+        brand: "Vue Mastery",
         product: "Donuts",
-        image: "./assets/51UDInCwZxL._SL500_.jpg",
-        onSale: false,
-        inStock: false,
+        selectedVariant: 0,
+        onSale: true,
         inventory: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 1,
                 variantColor: "chocolate",
-                variantImage: "./assets/51UDInCwZxL._SL500_.jpg"
+                variantImage: "./assets/51UDInCwZxL._SL500_.jpg",
+                variantQuanity: 10
             },
             {
                 variantId: 2,
                 variantColor: "pink",
-                variantImage: "./assets/reward-doughnut.png"
+                variantImage: "./assets/reward-doughnut.png",
+                variantQuanity: 0
             }
         ],
         cart: 0
@@ -25,11 +27,27 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
         },
-        addToCart2() {
-            this.cart += 1
+    },
+    computed: {
+        title() {
+            return this.product + " " + this.brand
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuanity
+        },
+        sale() {
+            if(this.onSale) {
+                return this.product + " " + this.brand + " on sale!"
+            }
+                return this.product + " " + this.brand + "are not on sale!"
         }
+
     }
 })
